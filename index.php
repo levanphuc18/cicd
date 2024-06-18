db324cfdeedb88a0e1ca9bbf5f9a58d5725aae44
Le Van Phuc <levanphuc18112001@gmail.com>
2024-06-18 10:11:13 +0700

    SSa

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ define('WP_USE_THEMES', true);
 
 // Thêm một dòng trống để tách biệt giữa khối tiêu đề và nội dung chính của file
 // (Đây là phần bạn cần chỉnh sửa để thêm một dòng trống)
-echo "Hello, this is a custom message PHUC";
+echo "Hello, this is a custom message PHUCZZZZ";
 
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
